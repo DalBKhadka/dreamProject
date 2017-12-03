922dee1645d1070d2c366f7401a3a23b6dae4bd0
Dal <dalbkhadka1@gmail.com>
2017-12-03 14:50:47 +1100

    1

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>My Profile</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Web development tutorials, Learn html and css, Beginner's html css guide">
 </head>
 </html>

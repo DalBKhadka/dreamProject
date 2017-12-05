1ec671673b9e783bd6ed8474a409c7aa542201f6
Dal <dalbkhadka1@gmail.com>
2017-12-05 13:45:24 +1100

    1

--- index.php
+++ index.php
@@ -6,4 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Web development tutorials, Learn html and css, Beginner's html css guide">
 </head>
+<body>
+    <header>
+
+    </header>
+    <main class="container">
+        <div class="profileBox">
+            <div class="profileBox__image0/"'1></div>
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
 </html>
